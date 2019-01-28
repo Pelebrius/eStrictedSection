c79d1a9ffef1c784f23f4b608150a027391d2184
Thomas Devous <Wizwardguin@gmail.com>
2019-01-27 22:02:31 -0600

    Report Functionality Added Files

---
+++ Report.Designer.vb
@@ -0,0 +1,102 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Report
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
+        Me.txtReport = New System.Windows.Forms.TextBox()
+        Me.dgvReport = New System.Windows.Forms.DataGridView()
+        Me.lblReportTitle = New System.Windows.Forms.Label()
+        Me.txtDate = New System.Windows.Forms.TextBox()
+        CType(Me.dgvReport, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ReportViewer1
+        '
+        Me.ReportViewer1.Location = New System.Drawing.Point(0, 0)
+        Me.ReportViewer1.Name = "ReportViewer"
+        Me.ReportViewer1.Size = New System.Drawing.Size(396, 246)
+        Me.ReportViewer1.TabIndex = 0
+        '
+        'txtReport
+        '
+        Me.txtReport.BackColor = System.Drawing.Color.Goldenrod
+        Me.txtReport.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtReport.Location = New System.Drawing.Point(30, 78)
+        Me.txtReport.Multiline = True
+        Me.txtReport.Name = "txtReport"
+        Me.txtReport.ReadOnly = True
+        Me.txtReport.Size = New System.Drawing.Size(371, 393)
+        Me.txtReport.TabIndex = 2
+        '
+        'dgvReport
+        '
+        Me.dgvReport.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvReport.Location = New System.Drawing.Point(12, 12)
+        Me.dgvReport.Name = "dgvReport"
+        Me.dgvReport.Size = New System.Drawing.Size(20, 19)
+        Me.dgvReport.TabIndex = 0
+        Me.dgvReport.Visible = False
+        '
+        'lblReportTitle
+        '
+        Me.lblReportTitle.BackColor = System.Drawing.Color.Goldenrod
+        Me.lblReportTitle.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblReportTitle.Location = New System.Drawing.Point(12, 9)
+        Me.lblReportTitle.Name = "lblReportTitle"
+        Me.lblReportTitle.Size = New System.Drawing.Size(409, 33)
+        Me.lblReportTitle.TabIndex = 3
+        Me.lblReportTitle.Text = "Weekly Report: Student Book Assignments"
+        '
+        'txtDate
+        '
+        Me.txtDate.BackColor = System.Drawing.Color.Goldenrod
+        Me.txtDate.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDate.Location = New System.Drawing.Point(12, 52)
+        Me.txtDate.Name = "txtDate"
+        Me.txtDate.Size = New System.Drawing.Size(70, 20)
+        Me.txtDate.TabIndex = 4
+        '
+        'Report
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WindowsApplication1.My.Resources.Resources.BackgroundImage_Papyrus
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(428, 485)
+        Me.Controls.Add(Me.txtDate)
+        Me.Controls.Add(Me.lblReportTitle)
+        Me.Controls.Add(Me.txtReport)
+        Me.Controls.Add(Me.dgvReport)
+        Me.Name = "Report"
+        Me.Text = "Report"
+        CType(Me.dgvReport, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Private WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
+    Friend WithEvents txtReport As System.Windows.Forms.TextBox
+    Friend WithEvents dgvReport As System.Windows.Forms.DataGridView
+    Friend WithEvents lblReportTitle As System.Windows.Forms.Label
+    Friend WithEvents txtDate As System.Windows.Forms.TextBox
+End Class
---
+++ Report.vb
@@ -0,0 +1,32 @@
+﻿Public Class Report
+    Private Sub Report_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Me.txtDate.Text = Now.Date
+
+        'Order incoming Data Table
+        dgvReport.Sort(dgvReport.Columns(4), System.ComponentModel.ListSortDirection.Ascending)
+
+        'Report information in text box
+        'Only one name for redeamed books
+        Dim lbl As New Label
+        Dim currentName As String = Nothing
+        Dim newName As String
+
+        For Each row As DataGridViewRow In dgvReport.Rows
+            If Not row.IsNewRow Then
+
+                newName = (row.Cells("StudentName").Value.ToString)
+
+                If Equals(currentName, newName) = True Then
+                    Me.txtReport.AppendText(vbTab & row.Cells("BookTitle").Value.ToString & Environment.NewLine)
+                Else
+                    Me.txtReport.AppendText(row.Cells("StudentName").Value.ToString & Environment.NewLine)
+                    Me.txtReport.AppendText(vbTab & row.Cells("BookTitle").Value.ToString & Environment.NewLine)
+                End If
+
+                currentName = newName
+            End If
+        Next
+    End Sub
+
+
+End Class
